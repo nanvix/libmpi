6a0f9c6c428452b3494e98eb7245f97981cfe8e5
João Fellipe <felippe.uller@gmail.com>
2020-05-22 16:29:09 -0300

    Added MPI_Communicator_t Structure Definition

---
+++ include/mpi/communicator.h
@@ -0,0 +1,150 @@
+/*
+ * MIT License
+ *
+ * Copyright(c) 2011-2020 The Maintainers of Nanvix
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#ifndef NANVIX_MPI_COMMUNICATOR_H_
+#define NANVIX_MPI_COMMUNICATOR_H_
+
+#include <nanvix/hal.h>
+#include <mputil/object.h>
+#include <mpi/group.h>
+#include <mpi.h>
+
+/**
+ * @brief Struct that defines a mpi_communicator.
+ *
+ * @todo Insert a MPI_Errorhandler reference when impl. supports it.
+ */
+struct mpi_communicator_t
+{
+	object_t super;                    /* Base object class.              */
+
+	struct mpi_group_t *group;         /* Group associated with the comm. */
+	int my_rank;                       /* Local rank inside comm.         */
+	int pt2pt_cid;                     /* Point-to-point context ID.      */
+	int coll_cid;                      /* Collective context ID.          */
+	struct mpi_communicator_t *parent; /* Parent communicator.            */
+};
+
+typedef struct mpi_communicator_t mpi_communicator_t;
+
+/* Class declaration. */
+OBJ_CLASS_DECLARATION(mpi_communicator_t);
+
+/**
+ * @brief Allocates a new communicator.
+ *
+ * @param group_size Communicator group size.
+ *
+ * @returns A pointer to the new allocated communicator.
+ */
+extern mpi_communicator_t * mpi_comm_allocate(int group_size);
+
+/**
+ * @brief Frees the specified communicator.
+ *
+ * @param comm Communicator to be freed.
+ *
+ * @returns Upon successful completion, zero is returned with
+ * @p comm pointing to NULL. A negative error code is returned
+ * instead, without modifications on @p comm.
+ */
+extern int mpi_comm_free(mpi_communicator_t **comm);
+
+/**
+ * @brief Gets the local process rank in @p comm.
+ *
+ * @param comm Target communicator.
+ *
+ * @returns The local process rank in the communicator.
+ */
+static inline int mpi_comm_rank(mpi_communicator_t * comm)
+{
+    return (comm->my_rank);
+}
+
+/**
+ * @brief Gets the size of the communicator.
+ *
+ * @param comm Target communicator.
+ *
+ * @returns The number of processes that participates in @p comm.
+ */
+static inline int mpi_comm_size(mpi_communicator_t * comm)
+{
+    return (comm->group->size);
+}
+
+/**
+ * @brief Gets the communicator point-to-point cid.
+ *
+ * @param comm Target communicator.
+ *
+ * @returns The communicator's point-to-point context ID.
+ */
+static inline int mpi_comm_get_pt2pt_cid(mpi_communicator_t* comm)
+{
+    return (comm->pt2pt_cid);
+}
+
+/**
+ * @brief Gets the communicator collective cid.
+ *
+ * @param comm Target communicator.
+ *
+ * @returns The communicator's collective context ID.
+ */
+static inline int mpi_comm_get_coll_cid(mpi_communicator_t* comm)
+{
+    return (comm->coll_cid);
+}
+
+/**
+ * @brief Extracts the group associated with the communicator.
+ *
+ * @param comm  The target communicator.
+ * @param group A group pointer to reference the target group upon success.
+ *
+ * @returns Upon successful completion, MPI_SUCCESS is returned with the
+ * group reference on @p group. An MPI_ERROR_CODE is returned instead with
+ * group pointing to NULL.
+ */
+extern int mpi_comm_group(mpi_communicator_t *comm, mpi_group_t **group);
+
+/**
+ * @brief Initializes the communicators submodule.
+ *
+ * @returns Upon successful completion, zero is returned. A
+ * negative error code is returned instead.
+ */
+extern int mpi_comm_init(void);
+
+/**
+ * @brief Finalizes the communicators submodule.
+ *
+ * @returns Upon successful completion, zero is returned. A
+ * negative error code is returned instead.
+ */
+extern int mpi_comm_finalize(void);
+
+#endif /* NANVIX_MPI_COMMUNICATOR_H_ */
---
+++ src/mpi/communicator/communicator.c
@@ -0,0 +1,226 @@
+/*
+ * MIT License
+ *
+ * Copyright(c) 2011-2020 The Maintainers of Nanvix
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include <nanvix/hlib.h>
+#include <nanvix/ulib.h>
+#include <posix/errno.h>
+#include <mpi/communicator.h>
+
+PRIVATE void mpi_comm_construct(mpi_communicator_t *);
+PRIVATE void mpi_comm_destruct(mpi_communicator_t *);
+
+OBJ_CLASS_INSTANCE(mpi_communicator_t, &mpi_comm_construct, &mpi_comm_destruct,
+	               sizeof(mpi_communicator_t));
+
+/**
+ * @brief Predefined communicators.
+ */
+mpi_communicator_t _mpi_comm_world;
+mpi_communicator_t _mpi_comm_self;
+mpi_communicator_t _mpi_comm_null;
+
+/**
+ * @brief Communicator constructor.
+ */
+PRIVATE void mpi_comm_construct(mpi_communicator_t * comm)
+{
+	uassert(comm != NULL);
+
+	comm->group     = NULL;
+	comm->my_rank   = MPI_UNDEFINED;
+	comm->pt2pt_cid = MPI_UNDEFINED;
+	comm->coll_cid  = MPI_UNDEFINED;
+	comm->parent    = NULL;
+}
+
+/**
+ * @brief Communicator destructor.
+ *
+ * @note When cid allocation became dynamic, it should be released here.
+ */
+PRIVATE void mpi_comm_destruct(mpi_communicator_t * comm)
+{
+    uassert(comm != NULL);
+
+    /* Release the associated group. */
+    if (comm->group != NULL)
+    	OBJ_RELEASE(comm->group);
+
+    /* Release parent reference. */
+    if (comm->parent != NULL)
+    	OBJ_RELEASE(comm->parent);
+}
+
+/**
+ * @see mpi_comm_allocate() at communicator.h.
+ */
+PUBLIC mpi_communicator_t * mpi_comm_allocate(int group_size)
+{
+	mpi_communicator_t *new_comm;
+	mpi_group_t *group;
+
+	/* Bad group_size. */
+	if (group_size < 0)
+		return (NULL);
+
+	/* Allocates a new communicator. */
+	new_comm = OBJ_NEW(mpi_communicator_t);
+	if (new_comm == NULL)
+		return (NULL);
+
+	/* Allocates a new group. */
+	group = mpi_group_allocate(group_size);
+	if (group == NULL)
+	{
+		OBJ_RELEASE(new_comm);
+		return (NULL);
+	}
+
+	new_comm->group = group;
+
+	return (new_comm);
+}
+
+/**
+ * @see mpi_comm_free() at communicator.h.
+ */
+PUBLIC int mpi_comm_free(mpi_communicator_t **comm)
+{
+	/* Bad communicator. */
+	if (*comm == NULL)
+		return (-EINVAL);
+
+	OBJ_RELEASE(comm);
+	*comm = MPI_COMM_NULL;
+
+	return (0);
+}
+
+/**
+ * @see mpi_comm_group() at communicator.h.
+ */
+PUBLIC int mpi_comm_group(mpi_communicator_t *comm, mpi_group_t **group)
+{
+	/* Bad communicator. */
+	if (comm == NULL)
+		return (-EINVAL);
+
+	/* Bad group receptor. */
+	if (group == NULL)
+		return (-EINVAL);
+
+	/* Increments the group reference counter. */
+	OBJ_RETAIN(comm->group);
+	*group = comm->group;
+
+	return (0);
+}
+
+/**
+ * @see mpi_comm_init() at communicator.h.
+ *
+ * @todo See necessity of defining a communicators array.
+ * @todo See necessity of freeing pre-allocated comms in case of failure.
+ */
+PUBLIC int mpi_comm_init(void)
+{
+	mpi_group_t *group;
+	size_t size;
+	char name[PROCESS_NAME_MAX_LENGTH];
+
+	/* Gets the number of active processes. */
+	size = mpi_proc_count();
+
+	/* Setup MPI_COMM_WORLD. */
+	OBJ_CONSTRUCT(&_mpi_comm_world, mpi_communicator_t);
+	group = OBJ_NEW(mpi_group_t);
+
+	group->procs = (mpi_process_t **) umalloc(size * sizeof(mpi_process_t *));
+	if (group->procs == NULL)
+		return (-ENOMEM);
+
+	group->size = size;
+
+	for (unsigned int i = 0; i < size; ++i)
+	{
+		usprintf(name, "nanvix-process-%d", i);
+		group->procs[i] = process_lookup(name);
+
+		uassert(group->procs[i] != NULL);
+	}
+
+	mpi_group_increment_proc_count(group);
+	mpi_group_set_rank(group, process_local());
+
+	_mpi_comm_world.group     = group;
+	_mpi_comm_world.my_rank   = group->my_rank;
+	_mpi_comm_world.pt2pt_cid = 0;
+	_mpi_comm_world.coll_cid  = 1;
+
+	/* Setup MPI_COMM_SELF. */
+	OBJ_CONSTRUCT(&_mpi_comm_self, mpi_communicator_t);
+	group = OBJ_NEW(mpi_group_t);
+
+	group->procs = (mpi_process_t **) umalloc(sizeof(mpi_process_t *));
+	if (group->procs == NULL)
+		return (-ENOMEM);
+
+	group->size = 1;
+	group->procs[0] = process_local();
+
+	mpi_group_increment_proc_count(group);
+	mpi_group_set_rank(group, process_local());
+
+	_mpi_comm_self.group     = group;
+	_mpi_comm_self.my_rank   = group->my_rank;
+	_mpi_comm_self.pt2pt_cid = 2;
+	_mpi_comm_self.coll_cid  = MPI_UNDEFINED;
+
+	/* Setup MPI_COMM_NULL. */
+	OBJ_CONSTRUCT(&_mpi_comm_null, mpi_communicator_t);
+	_mpi_comm_null.group     = MPI_GROUP_NULL;
+	_mpi_comm_null.my_rank   = MPI_PROC_NULL;
+	OBJ_RETAIN(MPI_GROUP_NULL);
+	
+	return (0);
+}
+
+/**
+ * @see mpi_comm_finalize() at communicator.h.
+ *
+ * @todo If define a communicators list we should free it here.
+ */
+PUBLIC int mpi_comm_finalize(void)
+{
+	/* Destruct MPI_COMM_SELF. */
+	OBJ_DESTRUCT(&_mpi_comm_self);
+
+	/* Destruct MPI_COMM_WORLD. */
+	OBJ_DESTRUCT(&_mpi_comm_world);
+
+	/* Destruct MPI_COMM_NULL. */
+	OBJ_DESTRUCT(&_mpi_comm_null);
+
+	return (0);
+}
